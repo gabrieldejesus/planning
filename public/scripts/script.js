e035cd97b59d97f03bd7c262114e0eb58e4f36d3
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-06-30 22:02:14 -0300

    feature to remove dark mode

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -27,8 +27,27 @@ function changeThemeDark() {
     document.querySelector('footer').classList.add('dark-color');
     document.getElementById('menu-window').style.background = ('#FFFFFF33');
     document.querySelector('aside').classList.add('dark-color');
-    document.querySelector('li').classList.add('dark-color');
+    document.getElementById('menu-option').classList.add('dark-color');
+}
 
-
-
+function changeThemeLight() {
+    // Header
+    document.getElementById('change-theme-light').style.display = 'flex'; // ativar o botão de thema light
+    document.getElementById('change-theme-dark').style.display = 'none'; // desativar o botão de thema dark
+    document.getElementById('open-menu').src = '/public/images/open-menu.webp'; // botão de menu lateral
+    document.getElementById('search-button').src = '/public/images/search.webp'; // icone de pesquisa
+    document.getElementById('search').style.borderBottom = '';
+    document.getElementById('search').style.color = '';
+    document.querySelector('header').classList.remove('dark-color');
+    document.querySelector('a').classList.remove('dark-color');
+    
+    // Main
+    document.querySelector('main').classList.remove('dark-color');
+    document.getElementById('title').style.color = ('');
+    document.getElementById('widget-body').classList.remove('black-color');
+    document.getElementById('copy').style.color = ('');
+    document.querySelector('footer').classList.remove('dark-color');
+    document.getElementById('menu-window').style.background = ('');
+    document.querySelector('aside').classList.remove('dark-color');
+    document.getElementById('menu-option').classList.remove('dark-color');
 }
